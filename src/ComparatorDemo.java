dfabea2dcd1d8de87d4406a315be0f98cc970d28
Donquixote <826714738@qq.com>
2022-08-24 07:09:41 +0800

    Comparator

---
+++ src/ComparatorDemo.java
@@ -0,0 +1,12 @@
+/**
+ * @author Donquixote
+ * @date 2022/08/24
+ **/
+public class ComparatorDemo {
+
+    // reference: https://blog.csdn.net/u014042066/article/details/76248692
+    // https://rumenz.com/java-topic/java8/using-comparator-becomes-easier-with-lambda-expressions-java-8/index.html
+    // https://www.baeldung.com/java-8-comparator-comparing
+
+
+}
